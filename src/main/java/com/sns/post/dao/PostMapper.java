ed84c4a3f9c32e33c9e704c027096ac39b7746e2
shing <test@naver.com>
2023-07-14 16:57:06 +0900

    초기환경셋팅

---
+++ src/main/java/com/sns/post/dao/PostMapper.java
@@ -0,0 +1,13 @@
+package com.sns.post.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostMapper {
+	
+	public List<Map<String, Object>> selectPostList();
+
+}
---
+++ src/main/java/com/sns/test/TestController.java
@@ -0,0 +1,58 @@
+package com.sns.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sns.post.dao.PostMapper;  
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	private PostMapper postMapper;
+	
+	@ResponseBody
+	@RequestMapping("/test1")
+	public String test1() {
+		return "hello world";
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping("/test2")
+	public Map<String, Object> test2() {
+
+		 Map<String, Object> map = new HashMap<>();
+		 
+		 map.put("1", 111);
+		 map.put("2", 222);
+		 map.put("3", 333);
+		 
+		 return map;
+		
+	}
+	
+	@RequestMapping("/test3")
+	public String test3() {
+
+		 return "test/test3";
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping("/test4")
+	public List<Map<String, Object>> test4() {
+		
+		
+		return postMapper.selectPostList();
+
+		
+	}
+
+}
